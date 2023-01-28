ae8272e2abf407f866a7a4c5b5d576e950f5e129
yarlaaguiar <119678710+yarlaaguiar@users.noreply.github.com>
2023-01-28 19:41:19 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// Elementos
+const input = document.querySelector('input');
+const lista = document.querySelector('ul');
+
+// Funções
+
+function handleKeyUp(e) {
+    if (e.key === 'Enter') {
+        const newLi = document.createElement('li');
+        newLi.innerHTML = input.value ;
+        lista.appendChild(newLi);
+    }
+}
+
+// Eventos
+
+input.addEventListener('keyup', handleKeyUp);
+
